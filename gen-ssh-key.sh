ec39c92d53ae81fa4f3aa5423e479b65c4c10eab
Emily Eubanks <shrimptankzilla@gmail.com>
2020-11-13 15:08:01 -0500

    Create gen-ssh-key.sh

---
+++ gen-ssh-key.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# INSTRUCTIONS
+# sudo gen-ssh-key.sh example@gmail.com
+
+if [ ! -n "$1" ] ; then
+	echo 'Missing argument: email_address'
+	exit 1
+fi
+
+if [ "$(id -u)" != "0" ] ; then
+	echo "You must be root to run this script."
+	exit 2
+fi
+
+email_addr=$1
+ssh-keygen -t ed25519 -C "$email_addr"
+ssh-add ~/.ssh/id_rsa
+echo "New SSH key added: ~/.ssh/id_rsa"
